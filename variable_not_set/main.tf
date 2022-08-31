c461c4e48beb8203e93997450ee2a5d704130d96
Kateryna Gubchenko <79134828+kategubchenko@users.noreply.github.com>
2022-08-31 23:23:18 +0100

    Update main.tf

--- variable_not_set/main.tf
+++ variable_not_set/main.tf
@@ -2,14 +2,13 @@ resource "null_resource" "wait" {
   triggers = {
     run_id = var.run_id
   }
-  provisioner "local-exec" {
-    command = "sleep ${var.sleep_time}"
-  }
 }
 
 variable "run_id" {
   sensitive = true
 }
-variable "sleep_time" {
-  default = 610
+
+output "output_run_id" {
+  value = var.run_id
+  sensitive = true
 }
