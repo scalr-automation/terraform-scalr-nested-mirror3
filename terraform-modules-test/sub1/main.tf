1d993ab958308b0e396edd744cc3220fd5ac0cb7
Kostya Kotov <38981440+k-kotov@users.noreply.github.com>
2022-09-12 15:55:13 +0300

    Create main.tf

---
+++ terraform-modules-test/sub1/main.tf
@@ -0,0 +1,14 @@
+resource "null_resource" "res" {
+  count = 30
+  triggers = {
+    run_id = var.run_id
+  }
+  provisioner "local-exec" {
+    command = "env"
+  }
+}
+
+variable "run_id" {
+  default = "run_id_2"
+  sensitive = true
+}
