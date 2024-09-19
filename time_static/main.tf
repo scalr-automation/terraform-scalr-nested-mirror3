d2a8e51ea9c582bce18ce7c6634b98a7861edc9c
Vladyslav Vlasenko <fl4shqq@gmail.com>
2024-09-19 17:48:25 +0300

    Update main.tf

--- time_static/main.tf
+++ time_static/main.tf
@@ -1,13 +1,17 @@
+variable "time_version" {
+  description = "Time provider version"
+  type        = string
+  default     = ""
+}
+
 terraform {
   required_providers {
     time = {
       source  = "hashicorp/time"
-      version = var.time_version
+      # If the variable is set, use it; otherwise, use the default version "0.12.0"
+      version = var.time_version != "" ? var.time_version : "0.12.0"
     }
   }
 }
 
-variable "time_version" {
-  description = "Time provider version"
-  type        = string
-}
+resource "time_static" "example" {}
