a7a1f0a17a5fc96ff88fef78a2b401ef386aa0b5
Vladyslav Vlasenko <fl4shqq@gmail.com>
2024-09-19 17:36:02 +0300

    Update main.tf

--- time_static/main.tf
+++ time_static/main.tf
@@ -1,14 +1,15 @@
-locals {
-  time_provider_version = var.time_version
-}
-
 terraform {
   required_providers {
     time = {
       source  = "hashicorp/time"
-      version = "${local.time_provider_version}"
+      version = "0.12.0"
     }
   }
+}
+
+resource "tls_private_key" "ecdsa-p384-example" {
+  algorithm   = "ECDSA"
+  ecdsa_curve = "P384"
 }
 
 resource "time_static" "example" {}
