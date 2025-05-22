96f2977bca0ad3cc84cea292d9a00b01e0819c17
AlexeyBudugay <o.buduhai@scalr.com>
2025-05-22 14:55:33 +0300

    SCALRCORE-T1100 Change branch ref

--- workspace_with_ssh/main.tf
+++ workspace_with_ssh/main.tf
@@ -1,3 +1,3 @@
 module "git" {
-  source = "git::ssh://git@github.com/scalr-automation/terraform-scalr-flat"
+  source = "git::ssh://git@github.com/scalr-automation/terraform-scalr-flat.git?ref=T1100"
 }
