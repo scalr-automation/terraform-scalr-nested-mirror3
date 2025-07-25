6814cb989052fedb53f7008cf2c060c8cfdd2cfe
Vladyslav Vlasenko <v.vlasenko@scalr.com>
2025-07-25 19:08:33 +0300

    Replace bucket with policy to avoid unique name issues

--- aws_default_tags/main.tf
+++ aws_default_tags/main.tf
@@ -8,6 +8,19 @@ provider "aws" {
     }
   }
 
-resource "aws_s3_bucket" "test" {
-  bucket = "scalr-test"
+resource "aws_iam_policy" "safe_policy_east" {
+  name        = "safe-policy-east"
+  description = "A policy that passes all Checkov checks - East"
+  
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Action    = ["s3:GetObject"]
+      Resource  = ["arn:aws:s3:::example-bucket-east/specific-path/*"]
+      Condition = {
+        IpAddress = {"aws:SourceIp" = ["192.0.2.0/24"]}
+      }
+    }]
+  })
 }
