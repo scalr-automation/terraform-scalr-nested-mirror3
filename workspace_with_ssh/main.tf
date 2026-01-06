1e1a0f9bef613a089b401b3940863e089993d918
Andrii Balakhtar <abalakh@users.noreply.github.com>
2026-01-06 16:39:00 +0000

    try another path format

--- workspace_with_ssh/main.tf
+++ workspace_with_ssh/main.tf
@@ -1,3 +1,3 @@
 module "git" {
-  source = "git::ssh://git@gitlab.main.scalr.dev:scalr-automation/terraform-scalr-flat.git"
+  source = "git::ssh://git@gitlab.main.scalr.dev/scalr-automation/terraform-scalr-flat.git"
 }
