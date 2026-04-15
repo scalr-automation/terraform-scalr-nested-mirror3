f8cc8d8ba8a7789f8247c761f44af86eca2518fb
Vladyslav Vlasenko <v.vlasenko@scalr.com>
2026-04-15 19:27:14 +0300

    Add output for current AWS account ID

--- aws_credential_source/main.tf
+++ aws_credential_source/main.tf
@@ -2,4 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_caller_identity" {}
+data "aws_caller_identity" "current" {}
+
+output "account_id" { 
+  value = data.aws_caller_identity.current.account_id 
+}
